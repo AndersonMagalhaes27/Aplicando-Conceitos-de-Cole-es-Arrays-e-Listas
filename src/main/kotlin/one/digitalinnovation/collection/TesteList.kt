2df557765db98766b56682053cc08b2e90ff5f50
AndersonMagalhaes27 <andersonmagalhaes027@gmail.com>
2021-07-05 11:28:49 -0300

    Desenvolvimento com listof groupbY, sortedby

--- src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -1,20 +1,30 @@
 package one.digitalinnovation.collection
 
 fun main() {
-    val Joao = funcionario ("Joao", 5000.0)
-    val Maria = funcionario ("Maria", 3000.0)
-    val Luiz = funcionario ("Luiz", 2000.0)
+    val Joao = funcionario ("Joao", 5000.0, "Clt")
+    val Maria = funcionario ("Maria", 3000.0,"Pj")
+    val Luiz = funcionario ("Luiz", 2000.0,"Clt")
 
     val funcionarios = listOf(Joao,Maria,Luiz)
 
     funcionarios.forEach { println(it) }
-    println("========================")
+
+            println("========================")
+
     println(funcionarios.find { it.nome == "Maria" })
 
+            println("========================")
+
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+            println("========================")
+
+    funcionarios.groupBy { it.contratacao }.forEach{println(it)}
 }
+
 data class funcionario (
     val nome:String,
-    val salario:Double
+    val salario:Double,
+    val contratacao:String
 ){
     override fun toString():String =
         "Nome: $nome     Salario: $salario ".trimIndent()
